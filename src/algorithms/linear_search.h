c034eca4edf8a6cd3a9916aa9aad337616b3f9d3
shofy <shofy44@mhs.usk.ac.id>
2026-06-03 18:35:42 +0700

    Add linear search header for searching warga by name

---
+++ src/algorithms/linear_search.h
@@ -0,0 +1,9 @@
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
+
+#include "warga.h"
+
+// Mencari warga berdasarkan nama menggunakan linear search
+Node* search_warga(Node *head, const char *nama);
+
+#endif
